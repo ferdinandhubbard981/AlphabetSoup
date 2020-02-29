0605e30666d2258fbbc4a37449d550af291ba6af
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2020-02-29 09:51:16 +0000

    sorts a string in alphabetical order

---
+++ AlphabetSoup/AlphabetSoup/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AlphabetSoup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your string:");
+            Console.WriteLine("\noutput: " + AlphabetSoup(Console.ReadLine()));
+
+            Console.ReadLine();
+        }
+
+
+        static string AlphabetSoup(string input)
+        {
+            input = input.ToLower();
+            string output = string.Empty;
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                for (int x = 0; x < input.Length; x++)
+                {
+                    if (alphabet[i] == input[x])
+                    {
+                        output += alphabet[i];
+                    }
+                }
+            }
+
+            return output;
+        }
+    }
+}
